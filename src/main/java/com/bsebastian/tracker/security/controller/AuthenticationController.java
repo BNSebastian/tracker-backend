fe150baf6b1cfa69e5893729152f648dab57b59f
BNSebastian <bucovala.sebastian@protonmail.com>
2024-02-05 21:09:41 +0200

    update

--- src/main/java/com/bsebastian/tracker/security/controller/AuthenticationController.java
+++ src/main/java/com/bsebastian/tracker/security/controller/AuthenticationController.java
@@ -4,9 +4,11 @@ import com.bsebastian.tracker.security.model.AuthenticationRequest;
 import com.bsebastian.tracker.security.model.AuthenticationResponse;
 import com.bsebastian.tracker.security.service.AuthenticationService;
 import com.bsebastian.tracker.security.model.RegisterRequest;
+import com.bsebastian.tracker.security.service.JwtService;
 import com.bsebastian.tracker.security.service.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -17,7 +19,9 @@ import java.util.Map;
 public class AuthenticationController {
 
     private final AuthenticationService authService;
+    private final JwtService jwtService;
     private final UserServiceImpl userService;
+
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
         return ResponseEntity.ok(authService.register(request));
@@ -28,9 +32,16 @@ public class AuthenticationController {
         return ResponseEntity.ok(authService.authenticate(request));
     }
 
-    @PostMapping("/isAdmin")
+    @GetMapping("/isAdmin")
     public ResponseEntity<Boolean> isAdmin(@RequestBody Map<String, Long> body) {
         Long userId = body.get("userId");
         return ResponseEntity.ok(userService.checkIfAdmin(userId));
     }
+
+    @GetMapping("/getUsername")
+    public ResponseEntity<String> getUsername(@RequestBody String authToken, UserDetails user) {
+        jwtService.isTokenValid(authToken, user);
+        return ResponseEntity.ok(jwtService.extractUsername(authToken));
+    }
+
 }
